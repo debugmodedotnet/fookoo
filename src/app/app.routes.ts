bd58b386d6c0ca85620d5c3a716f31c407c05ef8
Ariba <ariba.madakia@gmail.com>
2024-06-23 17:47:44 +0530

    Event-Details Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { EventDetailsComponent } from './event-details/event-details.component';
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
@@ -7,7 +8,9 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'signup', loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent) },
     { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
+    { path: 'profile', loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent) },
     { path: 'events', loadComponent: () => import('./event/event.component').then(m => m.EventComponent) },
+    { path: 'event-details', loadComponent: () => import('./event-details/event-details.component').then(m => m.EventDetailsComponent) },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 

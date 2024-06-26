859b48306032161d363272bac648e2bd273ef063
Rajnandini3847 <rajnandinitiwari384@gmail.com>
2024-06-26 18:19:44 +0530

    Created Instructor details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,7 @@ export const routes: Routes = [
     { path: 'create-event', loadComponent: () => import('./create-event/create-event.component').then(m => m.CreateEventComponent) },
     { path: 'create-instructor', loadComponent: () => import('./create-instructor/create-instructor.component').then(m => m.CreateInstructorComponent) },
     { path: 'event-details', loadComponent: () => import('./event-details/event-details.component').then(m => m.EventDetailsComponent) },
+    { path: 'instructor-details', loadComponent:() =>import('./instructor-details/instructor-details.component').then(m=>m.InstructorDetailsComponent)}
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 

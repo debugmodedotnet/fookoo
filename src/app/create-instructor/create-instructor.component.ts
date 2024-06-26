a28c10f174b1a7c0c22ffe4c229805a130b87d30
Rajnandini3847 <rajnandinitiwari384@gmail.com>
2024-06-26 16:01:21 +0530

    designed create instructor page

--- src/app/create-instructor/create-instructor.component.ts
+++ src/app/create-instructor/create-instructor.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-instructor',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-instructor.component.html',
-  styleUrl: './create-instructor.component.scss'
+  styleUrl: './create-instructor.component.scss',
 })
 export class CreateInstructorComponent {
+  instructorForm: FormGroup;
+  private firestore = inject(AngularFirestore);
+
+  constructor() {
+    this.instructorForm = new FormGroup({
+      Name: new FormControl('', Validators.required),
+      Position: new FormControl(''),
+      Skills: new FormControl('', Validators.maxLength(200)),
+      Bio: new FormControl('', Validators.maxLength(200)),
+      X: new FormControl(''),
+      Facebook: new FormControl(''),
+      Instagram: new FormControl(''),
+      LinkedIn: new FormControl(''),
+    });
+  }
+
+  
+
+  onSubmit() {
+    if (this.instructorForm.valid) {
+      this.firestore.collection('instructor').add(this.instructorForm.value)
+        .then(docRef => {
+          console.log(`Document written with ID: ${docRef.id}`);
+        })
+        .catch(error => {
+          console.error("Error adding document: ", error);
+        });
+    }
+  }
 
 }

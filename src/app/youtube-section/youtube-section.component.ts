41dffb4d9fc99a5319182649d8f17639482eb98e
Ariba <ariba.madakia@gmail.com>
2024-06-27 21:10:39 +0530

    Homepage changes

--- src/app/youtube-section/youtube-section.component.ts
+++ src/app/youtube-section/youtube-section.component.ts
@@ -1,13 +1,33 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit, inject } from '@angular/core';
+import { DatePipe, NgFor } from '@angular/common';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { RouterModule } from '@angular/router';
+import { IYoutubeVideos } from '../modules/home-youtube';
 
 @Component({
   selector: 'app-youtube-section',
   standalone: true,
-  imports: [],
+  imports: [NgFor, DatePipe, RouterModule],
   templateUrl: './youtube-section.component.html',
   styleUrl: './youtube-section.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class YoutubeSectionComponent {
+export class YoutubeSectionComponent implements OnInit {
+
+  youtubeVideos: IYoutubeVideos[] = [];
+  private firestore = inject(AngularFirestore);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.firestore.collection('homepagedata').doc('learn-youtube').collection('youtube-videos').valueChanges().subscribe(youtubeVideos => {
+      console.log(youtubeVideos);
+      this.youtubeVideos = youtubeVideos as IYoutubeVideos[];
+    });
+  }
 
 }

eb24c9df18147f31c9b9d49f0e194f36080b394e
Ariba <ariba.madakia@gmail.com>
2024-07-01 20:14:13 +0530

    Resolved errors

--- src/app/instructor-details/instructor-details.component.ts
+++ src/app/instructor-details/instructor-details.component.ts
@@ -8,10 +8,10 @@ interface Instructor {
   Email: string;
   InstructorImg: string;
   Position: string;
-  'Skill-1': string;
-  'Skill-2': string;
-  'Skill-3': string;
-  'Skill-4': string;
+  skill1: string;
+  skill2: string;
+  skill3: string;
+  skill4: string;
   LinkedIn: string;
   Twitter: string;
   github: string;
@@ -26,19 +26,20 @@ export class InstructorDetailsComponent implements OnInit {
   instructor: Instructor | undefined;
   skills: string[] = [];
 
-  constructor(private route: ActivatedRoute, private db: AngularFireDatabase) {}
+  constructor(private route: ActivatedRoute, private db: AngularFireDatabase) { }
 
   ngOnInit(): void {
     const instructorId = this.route.snapshot.paramMap.get('id');
+
     if (instructorId) {
       this.db.object<Instructor>(`instructors/${instructorId}`).valueChanges().subscribe(data => {
         if (data) {
           this.instructor = data;
           this.skills = [
-            data['Skill-1'], 
-            data['Skill-2'], 
-            data['Skill-3'], 
-            data['Skill-4']
+            data['skill1'],
+            data['skill2'],
+            data['skill3'],
+            data['skill4']
           ].filter(skill => skill);
         }
       });

d8105c0286d2419e681581c758d08f34583341de
Rajnandini3847 <rajnandinitiwari384@gmail.com>
2024-07-01 18:34:31 +0530

    Instructor detail page update

--- src/app/instructor-details/instructor-details.component.ts
+++ src/app/instructor-details/instructor-details.component.ts
@@ -1,38 +1,47 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { IInstructor } from '../modules/instructors';
+interface Instructor {
+  Name: string;
+  Bio: string;
+  Email: string;
+  InstructorImg: string;
+  Position: string;
+  'Skill-1': string;
+  'Skill-2': string;
+  'Skill-3': string;
+  'Skill-4': string;
+  LinkedIn: string;
+  Twitter: string;
+  github: string;
+}
 
 @Component({
   selector: 'app-instructor-details',
-  standalone: true,
-  imports: [CommonModule, NgFor, RouterModule],
   templateUrl: './instructor-details.component.html',
-  styleUrl: './instructor-details.component.scss'
+  styleUrls: ['./instructor-details.component.scss']
 })
 export class InstructorDetailsComponent implements OnInit {
+  instructor: Instructor | undefined;
+  skills: string[] = [];
 
-  instructor?: IInstructor;
-
-  private firestore = inject(AngularFirestore);
-  private route = inject(ActivatedRoute);
+  constructor(private route: ActivatedRoute, private db: AngularFireDatabase) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const instructorId = params.get('id');
-      if (instructorId) {
-        this.getInstructorDetails(instructorId);
-      }
-    });
+    const instructorId = this.route.snapshot.paramMap.get('id');
+    if (instructorId) {
+      this.db.object<Instructor>(`instructors/${instructorId}`).valueChanges().subscribe(data => {
+        if (data) {
+          this.instructor = data;
+          this.skills = [
+            data['Skill-1'], 
+            data['Skill-2'], 
+            data['Skill-3'], 
+            data['Skill-4']
+          ].filter(skill => skill);
+        }
+      });
+    }
   }
-
-  getInstructorDetails(id: string) {
-    this.firestore.collection('instructor').doc(id).valueChanges().subscribe(instructor => {
-      console.log("Instructor details:", instructor);
-      this.instructor = instructor as IInstructor;
-    });
-  }
-
 }

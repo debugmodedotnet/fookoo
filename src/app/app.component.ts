706b21bdb8c87affb0807a687230752e6ff5b96d
Ariba <ariba.madakia@gmail.com>
2024-07-02 11:14:50 +0530

    Added Top Scroll Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,12 +12,36 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'NomadCoder';
+  scrollTop: HTMLElement | null = null;
 
   constructor(
     private router: Router
   ) { }
+
+  ngOnInit(): void {
+    this.scrollTop = document.querySelector('.scroll-top');
+
+    if (this.scrollTop) {
+      this.toggleScrollTop();
+      window.addEventListener('scroll', () => this.toggleScrollTop());
+      this.scrollTop.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      });
+    }
+  }
+
+  toggleScrollTop(): void {
+    if (this.scrollTop) {
+      this.scrollTop.classList.toggle('active', window.scrollY > 100);
+    }
+  }
 
   isLoginPage(): boolean {
     return this.router.url === '/login' || this.router.url === '/signup';

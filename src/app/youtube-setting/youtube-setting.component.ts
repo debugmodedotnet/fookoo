c7772f0cb2cc3d9f57d88b9504d75437170b4a33
kashishvermaa1 <kashishvermaa.29@gmail.com>
2024-07-02 11:26:02 +0530

    youtube-form

--- src/app/youtube-setting/youtube-setting.component.ts
+++ src/app/youtube-setting/youtube-setting.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { IYoutubeVideos } from '../modules/home-youtube';
 import { YoutubeVideoService } from '../youtube-video.service';
 import { ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-youtube-setting',
   standalone: true,
-  imports: [ ReactiveFormsModule, NgFor],
+  imports: [ ReactiveFormsModule, NgFor, NgIf],
   templateUrl: './youtube-setting.component.html',
   styleUrl: './youtube-setting.component.scss'
 })
@@ -16,6 +16,7 @@ export class YoutubeSettingComponent {
   videoForm: FormGroup;
   editMode = false;
   currentVideoId?: string;
+  formVisible = false;
 
   constructor(private youtubeVideoService: YoutubeVideoService, private fb: FormBuilder) {
     this.videoForm = this.fb.group({
@@ -29,39 +30,59 @@ export class YoutubeSettingComponent {
   }
 
   ngOnInit(): void {
-    this.youtubeVideoService.getVideos().subscribe(videos => {
-      this.videos = videos;
-    });
+    this.loadVideos();
+  }
+
+  loadVideos() {
+    this.youtubeVideoService.getVideos().subscribe(
+      videos => {
+        this.videos = videos;
+      },
+      error => {
+        console.error('Error loading videos:', error);
+      }
+    );
   }
 
   addOrUpdateVideo() {
     if (this.editMode && this.currentVideoId) {
-      this.youtubeVideoService.updateVideo(this.currentVideoId, this.videoForm.value).then(() => {
-        this.resetForm();
-      }).catch(error => {
-        console.error('Error updating video:', error);
-      });
+      this.updateVideo(this.currentVideoId, this.videoForm.value);
     } else {
-      this.youtubeVideoService.addVideo(this.videoForm.value).then(() => {
-        this.resetForm();
-      }).catch(error => {
-        console.error('Error adding video:', error);
-      });
+      this.addVideo(this.videoForm.value);
     }
+  }
+
+  addVideo(video: IYoutubeVideos) {
+    this.youtubeVideoService.addVideo(video).then(() => {
+      this.resetForm();
+      this.loadVideos(); // Reload videos after adding
+    }).catch(error => {
+      console.error('Error adding video:', error);
+    });
+  }
+
+  updateVideo(id: string, video: IYoutubeVideos) {
+    this.youtubeVideoService.updateVideo(id, video).then(() => {
+      this.resetForm();
+      this.loadVideos(); // Reload videos after update
+    }).catch(error => {
+      console.error('Error updating video:', error);
+    });
   }
 
   editVideo(video: IYoutubeVideos) {
     this.videoForm.patchValue(video);
     this.editMode = true;
     this.currentVideoId = video.id;
+    this.formVisible = true; // Show the form
   }
 
   deleteVideo(id: string | undefined) {
     if (id) {
       this.youtubeVideoService.deleteVideo(id).then(() => {
-        this.youtubeVideoService.getVideos().subscribe(videos => {
-          this.videos = videos;
-        });
+        this.loadVideos(); // Reload videos after deletion
+      }).catch(error => {
+        console.error('Error deleting video:', error);
       });
     } else {
       console.error('Video ID is undefined, cannot delete.');
@@ -79,5 +100,15 @@ export class YoutubeSettingComponent {
     });
     this.editMode = false;
     this.currentVideoId = undefined;
+    this.formVisible = false; // Hide the form
   }
+
+  showForm() {
+    this.formVisible = true;
+  }
+
+  hideForm() {
+    this.formVisible = false;
+  }
+
 }

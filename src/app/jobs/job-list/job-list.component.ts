bf3eec7eb89706557d13fc86bc65b67d2b9797c5
spector11 <nidhishnaghera3@gmail.com>
2024-07-10 14:29:16 +0530

    Fixed error in job-list.component.ts

--- src/app/jobs/job-list/job-list.component.ts
+++ src/app/jobs/job-list/job-list.component.ts
@@ -2,17 +2,22 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { Job } from '../../modules/job';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-job-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor,
+    AsyncPipe
+  ],
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.scss'
 })
 export class JobListComponent implements OnInit {
   @Output() selectJob = new EventEmitter<Job>();
-  jobs$: Observable<Job[]>;
+  jobs$?: Observable<Job[]>;
 
   constructor(private firestore: AngularFirestore) {}
 

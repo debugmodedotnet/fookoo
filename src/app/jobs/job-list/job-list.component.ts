172fbea360de96c184dc2af46617dc02b71d1d50
kashishvermaa1 <kashishvermaa.29@gmail.com>
2024-07-11 16:38:20 +0530

    filtertag

--- src/app/jobs/job-list/job-list.component.ts
+++ src/app/jobs/job-list/job-list.component.ts
@@ -1,5 +1,55 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, combineLatest, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Job } from '../../modules/job';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-job-list',
+  standalone: true,
+  imports: [NgIf, NgFor, AsyncPipe],
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.scss']
+})
+export class JobListComponent implements OnInit {
+  @Output() selectJob = new EventEmitter<Job>();
+  jobs$!: Observable<Job[]>;
+  tags: string[] = ['Angular', 'React', 'GenAI', 'JavaScript', 'TypeScript'];
+  selectedTag: string = '';
+  filteredJobs$!: Observable<Job[]>;
+
+  constructor(private firestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.jobs$ = this.firestore.collection<Job>('jobs').valueChanges();
+    this.filteredJobs$ = combineLatest([this.jobs$, of(this.selectedTag)]).pipe(
+      map(([jobs, tag]) => {
+        if (!tag) return jobs;
+        return jobs.filter(job => job.Tag === tag);
+      })
+    );
+  }
+
+  onTagFilterChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedTag = target.value;
+    this.filteredJobs$ = combineLatest([this.jobs$, of(this.selectedTag)]).pipe(
+      map(([jobs, tag]) => {
+        if (!tag) return jobs;
+        return jobs.filter(job => job.Tag === tag);
+      })
+    );
+  }
+
+  onJobClick(job: Job) {
+    this.selectJob.emit(job);
+  }
+}
+
+
+/*import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { Job } from '../../modules/job';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
@@ -29,4 +79,4 @@ export class JobListComponent implements OnInit {
   onJobClick(job: Job) {
     this.selectJob.emit(job);
   }
-}
+}*/

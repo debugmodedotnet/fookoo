232e7f2a5c0d8129db5060e8d18f12813bfd85a6
Ariba <ariba.madakia@gmail.com>
2024-07-19 12:16:20 +0530

    Minor changes

--- src/app/instructor-listing/instructor-listing.component.ts
+++ src/app/instructor-listing/instructor-listing.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { IInstructor } from '../modules/instructors';
+import { RouterModule } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-instructor-listing',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, RouterModule],
   templateUrl: './instructor-listing.component.html',
   styleUrl: './instructor-listing.component.scss'
 })
-export class InstructorListingComponent {
+export class InstructorListingComponent implements OnInit {
 
+  instructors: IInstructor[] = [];
+  private firestore = inject(AngularFirestore);
+
+  defaultImage = 'assets/images/home/defaultInstructor.jpg';
+
+  ngOnInit(): void {
+    this.getInstructors();
+  }
+
+  getInstructors() {
+    this.firestore.collection('instructor', ref => ref.limit(3)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as IInstructor;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    ).subscribe(instructors => {
+      console.log(instructors);
+      this.instructors = instructors as IInstructor[];
+    });
+  }
+  
 }

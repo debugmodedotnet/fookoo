e53e4cd40627c3b45cdb9bca9b0d9b42e3ac75d5
kashishvermaa1 <kashishvermaa.29@gmail.com>
2024-07-31 18:12:31 +0530

    salary

--- src/app/post-jobs/jobstep6/sal-validator.ts
+++ src/app/post-jobs/jobstep6/sal-validator.ts
@@ -5,11 +5,22 @@ import { AbstractControl,
 export const SalValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const minSal = control.get('MinSalary');
   const maxSal = control.get('MaxSalary');
-  if (minSal && maxSal && minSal.value >= maxSal.value) {
-    return {
-      salError: true,
-    };
+
+  if (minSal && maxSal) {
+    const minSalValue = Number(minSal.value);
+    const maxSalValue = Number(maxSal.value);
+
+    // Validate only if both values are provided
+    if (!isNaN(minSalValue) && !isNaN(maxSalValue)) {
+      if (minSalValue < 10000) {
+        return { minSalaryError: true }; // Min salary error
+      }
+      if (minSalValue >= maxSalValue) {
+        return { salError: true }; // Min salary should be less than max salary
+      }
+    }
   }
+  
   return null;
 };
 

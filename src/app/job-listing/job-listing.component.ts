407e65fd8ee1995a114dbd513286effe02e7a886
Ariba <ariba.madakia@gmail.com>
2024-08-30 12:57:11 +0530

    Post Job changes

--- src/app/job-listing/job-listing.component.ts
+++ src/app/job-listing/job-listing.component.ts
@@ -3,8 +3,10 @@ import { Observable, of } from 'rxjs';
 import { JobService } from '../services/job.service';
 import { Job } from '../modules/job';
 import { AsyncPipe } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { IUser } from '../modules/user';
 
 @Component({
   selector: 'app-job-listing',
@@ -17,18 +19,26 @@ export class JobListingComponent implements OnInit {
 
   jobs$: Observable<Job[]> = of([]);
   filteredJobs: Job[] = [];
+  user?: IUser;
 
   companyFilter = '';
   positionFilter = '';
   remoteFilter = false;
 
-
   private jobService = inject(JobService);
+  private userService = inject(UserService);
+  public router = inject(Router);
 
   ngOnInit() {
     this.jobs$ = this.jobService.getJobs();
     this.jobs$.subscribe(jobs => {
       this.filteredJobs = jobs;
+    });
+
+    this.userService.getCurrentUser().subscribe((user) => {
+      if (user) {
+        this.user = user;
+      }
     });
   }
 

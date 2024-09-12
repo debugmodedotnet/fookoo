155a0b0f111ba34507e3ae0a3d5b300f0cf6e9fd
Ariba <ariba.madakia@gmail.com>
2024-09-12 14:35:26 +0530

    Added Newsletter & Fixed job sorting

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,12 @@ import { InstructorsComponent } from '../instructors/instructors.component';
 import { YoutubeSectionComponent } from '../youtube-section/youtube-section.component';
 import { JobsSectionComponent } from '../jobs-section/jobs-section.component';
 import { HeroTechComponent } from '../hero-tech/hero-tech.component';
+import { NewsletterComponent } from '../newsletter/newsletter.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, HeroTechComponent, EventComponent, InstructorsComponent, YoutubeSectionComponent, JobsSectionComponent],
+  imports: [HeroComponent, HeroTechComponent, EventComponent, InstructorsComponent, YoutubeSectionComponent, JobsSectionComponent, NewsletterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
